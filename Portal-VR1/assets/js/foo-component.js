0c2236c7afcb95ce440d5747fb620d17d92ff767
jalvarez212 <johncarlosalvarez1@gmail.com>
2018-12-13 18:51:16 -0500

    Add files via upload

---
+++ Portal-VR1/assets/js/foo-component.js
@@ -0,0 +1,10 @@
+// Registering component in foo-component.js
+AFRAME.registerComponent('foo', {
+  schema: { url(assets/models/scene.glft) },
+  init: function () {},
+  update: function () {},
+  tick: function () {},
+  remove: function () {},
+  pause: function () {},
+  play: function () {}
+});
